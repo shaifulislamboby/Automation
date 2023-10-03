67e3b2eed38c60fa378dace07c5e70c9cd583c33
Md Shaiful Islam <sislam@cocus.com>
2023-10-03 12:07:35 +0200

    first commit

---
+++ src/main/java/com/example/networkautomation/NetworkAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.networkautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/networkautomation/NetworkAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.networkautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
