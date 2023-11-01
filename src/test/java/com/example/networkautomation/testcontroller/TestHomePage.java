657d5f383df5a45a9e413a9806513eae803317f7
Md Shaiful Islam <sislam@cocus.com>
2023-11-01 20:05:37 +0100

    CU-86bwajbed - chore: add basic ci workflow to check code styling and run tests

--- src/test/java/com/example/networkautomation/testcontroller/TestHomePage.java
+++ src/test/java/com/example/networkautomation/testcontroller/TestHomePage.java
@@ -1,4 +1,6 @@
-package com.example.networkautomation.TestController;
+package com.example.networkautomation.testcontroller;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,28 +11,36 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-
+/**
+ * This is a Javadoc comment. It provides information about the method or class.
+ * You can include a description, parameter explanations, return value, exceptions, etc.
+ */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {"welcomeMessage=test"})
 public class TestHomePage {
     @Autowired
-    public TestRestTemplate template;
+    private TestRestTemplate template;
 
     @Test
-    public void getHello() throws Exception{
+    public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/", String.class);
         assertThat(response.getBody()).isEqualTo("test");
     }
-
+    /**
+     * This is a Javadoc comment. It provides information about the method or class.
+     * You can include a description, parameter explanations, return value, exceptions, etc.
+     */
     @Test
     public void testHomePageStatus() {
         HttpStatus statusCode = (HttpStatus) template.exchange("/", HttpMethod.GET, null, String.class).getStatusCode();
 
         assertThat(statusCode).isEqualTo(HttpStatus.OK);
     }
-
+    /**
+     * This is a Javadoc comment. It provides information about the method or class.
+     * You can include a description, parameter explanations, return value, exceptions, etc.
+     */
     @Test
     public void testNotFoundPage() {
         ResponseEntity<String> response = template.getForEntity("/nonexistent", String.class);
