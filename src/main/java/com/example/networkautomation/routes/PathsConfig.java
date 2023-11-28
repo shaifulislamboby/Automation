d62f4fe1c6d78012625c25819bd6875cb089005d
olisheikh <olisheikh707@gmail.com>
2023-11-28 22:37:52 +0600

    login page has been created

---
+++ src/main/java/com/example/networkautomation/routes/PathsConfig.java
@@ -0,0 +1,21 @@
+package com.example.networkautomation.routes;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class PathsConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
+
+
+
